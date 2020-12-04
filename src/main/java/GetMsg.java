a2f8cdfb5814e4db72aee6fdb59a1fd47551a92a
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 02:39:07 +0300

    Scripted auto-commit on change (2020-12-05 02:39:07) by gitwatch.sh

--- src/main/java/GetMsg.java
+++ src/main/java/GetMsg.java
@@ -1,2 +1,12 @@
 public class GetMsg {
+    private int packageID;
+
+    public GetMsg(int id) {
+        this.packageID = id;
+    }
+
+    public int getPackageID() {
+        return packageID;
+    }
+
 }
