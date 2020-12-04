78bcfc185fc253a88659388814d19f114b9d8f3d
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 00:48:15 +0300

    Scripted auto-commit on change (2020-12-05 00:48:15) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,21 @@
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
 public class Server {
 
     public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("ActSys");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        Server server = new Server(system);
 
+    }
 
+    private Server(final ActorSystem system) {
+        storeActor = system.actorOf(Props.create(**), "storeActor");
+        testActor = system.actorOf(Props.create(**), "testActor")
+        routerActor = system.actorOf()
     }
 }
