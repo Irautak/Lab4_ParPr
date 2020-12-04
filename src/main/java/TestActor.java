d5f46b8af61510d2cc49dbbc1e90ea966c66ad40
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 02:59:19 +0300

    Scripted auto-commit on change (2020-12-05 02:59:19) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -1,6 +1,16 @@
+import akka.actor.AbstractActor;
 import akka.actor.ActorSelection;
+import akka.japi.pf.ReceiveBuilder;
 
-public class TestActor {
+public class TestActor extends AbstractActor {
 
-    private ActorSelection storeActor = getContext().
+    private ActorSelection storeActor = getContext().actorSelection("/user/storeActor");
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                            .match()
+    }
+
+
 }
