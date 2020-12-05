7f90bf31edb354a1b3168d2431e2cd8918cf5b6c
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 03:59:11 +0300

    Scripted auto-commit on change (2020-12-05 03:59:11) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,14 +2,24 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
+import akka.pattern.PatternsCS;
 import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
-import java.net.http.HttpRequest;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+import java.util.regex.Pattern;
+
+import static akka.http.javadsl.server.Directives.*;
 
 public class Server {
     private ActorRef storeActor;
@@ -19,13 +29,23 @@ public class Server {
     private static final String SERV_NAME = "localhost";
     private static final int PORT = 8080;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("ActSys");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         Server server = new Server(system);
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> flow =
+        final Flow<HttpRequest, HttpResponse, NotUsed> flow = server.makeRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                flow,
+                ConnectHttp.toHost(SERV_NAME, PORT),
+                materializer
+        );
+        System.out.println("Server have been started");
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+        System.out.println("Server have been turned off");
 
     }
 
@@ -39,7 +59,16 @@ public class Server {
 
     private Route makeRoute() {
         return route (
-                
-        )
+                get(() ->
+                            parameter("packageID", (packageID) -> {
+                                CompletionStage<Object> result = PatternsCS.ask(storeActor,
+                                                                new GetMsg(Integer.parseInt(packageID)),
+                                        5000);
+                                return completeOKWithFuture(result, Jackson.marshaller());
+                            })),
+                post(() -> entity(Jackson.unmarshaller(TestGroupMsg.class), msg -> {
+                    testGroupActor.tell(msg, ActorRef.noSender());
+                    return complete("Test have been started");
+                })));
     }
 }
