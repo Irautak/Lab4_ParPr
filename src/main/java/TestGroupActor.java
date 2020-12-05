ea6dce7bc836d11bf70139c2f53c8a0c2e36ab0a
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 03:27:28 +0300

    Scripted auto-commit on change (2020-12-05 03:27:28) by gitwatch.sh

--- src/main/java/TestGroupActor.java
+++ src/main/java/TestGroupActor.java
@@ -1,2 +1,13 @@
-public class TestGroupActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+import akka.japi.pf.ReceiveBuilder;
+
+public class TestGroupActor extends AbstractActor {
+    private ActorSelection routerActor = getContext().actorSelection("/user/routerActor");
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                            .match()
+    }
 }
